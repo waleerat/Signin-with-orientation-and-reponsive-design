894c3f7ff22914aff6e08afef7e6bf36dbc55f60
Waleerat S. Gottlieb <waleerat@shortcut.no>
2021-09-24 19:03:24 +0200

    Initial Commit

---
+++ Signin-with-orientation-and-reponsive-design/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Signin-with-orientation-and-reponsive-design
+//
+//  Created by Waleerat Gottlieb on 2021-09-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Signin-with-orientation-and-reponsive-design/Signin_with_orientation_and_reponsive_designApp.swift
@@ -0,0 +1,17 @@
+//
+//  Signin_with_orientation_and_reponsive_designApp.swift
+//  Signin-with-orientation-and-reponsive-design
+//
+//  Created by Waleerat Gottlieb on 2021-09-24.
+//
+
+import SwiftUI
+
+@main
+struct Signin_with_orientation_and_reponsive_designApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
